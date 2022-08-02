82556bee1ad5e949a041f0a535c7f17a6a0c9425
鍾岳儒 <andy6756@eri.com.tw>
2022-08-02 11:07:44 +0800

    加入 Garmin Connect 路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  {
+    path: 'garminConnect',
+    redirectTo: 'GarminConnect'
+  },
+  {
+    path: 'GarminConnect',
+    loadChildren: () => import('./garmin-connect/garmin-connect.module').then(m => m.GarminConnectModule)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GarminConnectComponent } from './garmin-connect/garmin-connect.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GarminConnectComponent
   ],
   imports: [
     BrowserModule,
